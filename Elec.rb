f61cc9e497a03147fc7bc589221cb76df589d297
Hammie217 <hcsams1@sheffield.ac.uk>
2017-11-14 13:36:57 +0000

    menu

--- Elec.rb
+++ Elec.rb
@@ -1,5 +1,30 @@
-def sayHi
-  puts "Hello"
+def selection()
+  input = gets.chomp
+  system("clear")
+  unless(input=='1'||input=='2')
+    puts"Incorrect input"
+    drawMenu()
+  end
+  if input=='1'
+    pcm()
+  elsif input=='2'
+    lrcm()
+  end
 end
 
-sayHi
+def pcm()
+  puts "Passive components"
+end
+
+def lrcm()
+  puts "LRC networks"
+end
+
+def drawMenu()
+  puts "1.Passive components"
+  puts "2.LRC networks"
+  selection()
+end
+
+
+drawMenu()
